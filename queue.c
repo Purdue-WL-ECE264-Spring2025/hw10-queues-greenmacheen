c60d909fe1d6f712230874acc47aaec7477fdd6d
GreenMacheen <46729245+greenmacheen@users.noreply.github.com>
2025-04-04 17:27:08 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -10,7 +10,7 @@ void enqueue(struct queue *q, struct game_state state) {
 
 struct game_state dequeue(struct queue *q) { 
   
-  return (struct game_state)remove_from_tail(&(q->data)); 
+  return deserialize(remove_from_tail(&(q->data))); 
 }
 
 
