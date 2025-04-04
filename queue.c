6b1afa96b9d0b032912a66d4a9fff5b084951fc0
GreenMacheen <46729245+greenmacheen@users.noreply.github.com>
2025-04-04 17:05:01 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,15 +1,23 @@
 #include "queue.h"
 #include "tile_game.h"
+#include "linked_list.h"
 
 void enqueue(struct queue *q, struct game_state state) {
-  
+  struct linked_list *data = q -> data;
+  insert_at_tail(data, state);
+ 
 }
 
 struct game_state dequeue(struct queue *q) { 
+  struct linked_list *data = q -> data;
+ 
+  return remove_from_tail(data); 
+}
+
+
+int number_of_moves(struct game_state start) { 
+
   
-  return (struct game_state){0}; }
-
-
-int number_of_moves(struct game_state start) { return 0; 
+  return 0; 
                                               
                                              }
