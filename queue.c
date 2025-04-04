09fae2cafec3e40ef5cbde3ca5025086809d546d
GreenMacheen <46729245+greenmacheen@users.noreply.github.com>
2025-04-04 16:57:04 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,8 +1,15 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+  
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) { 
+  
+  return (struct game_state){0}; }
 
-int number_of_moves(struct game_state start) { return 0; }
+
+int number_of_moves(struct game_state start) { return 0; 
+                                              
+                                             }
