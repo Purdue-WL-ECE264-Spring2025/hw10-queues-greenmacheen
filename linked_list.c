01010f8c2f1e6aa9253238b94000d945ad239636
GreenMacheen <46729245+greenmacheen@users.noreply.github.com>
2025-04-04 16:44:11 -0400

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -54,12 +54,12 @@ size_t remove_from_tail(struct linked_list *list) {
 
 void free_list(struct linked_list list) {
   struct list_node temp = list.head;              //Format taken from insumity from StackOverflow - rewritten by me for this assignment
-                                       //I originally was writing it myself, but while researching to confirm I came across this and couldnt really forget it so I just cited it
+                                                  //I originally was writing it myself, but while researching to confirm I came across this and couldnt really forget it so I just cited it
    while (list.head != NULL)
     {
        temp = list.head;
-       list.head = head.next;
-       free(temp);
+       list.head = &temp.next;
+       free(*temp);
     }
 }
 
