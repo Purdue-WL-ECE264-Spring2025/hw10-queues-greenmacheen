b67a72dc26a25e1b4d05f96387d0072ecbda6fc8
GreenMacheen <46729245+greenmacheen@users.noreply.github.com>
2025-04-04 16:29:38 -0400

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -9,13 +9,13 @@ struct list_node *new_node(size_t value) {
 }
 
 void insert_at_head(struct linked_list *list, size_t value) {
-   struct list_node newNode = new_node(value);
+   struct list_node *newNode = new_node(value);
    newNode->next = list.head;
   
 }
 
 void insert_at_tail(struct linked_list *list, size_t value) {
-  struct list_node newNode = new_node(value);
+  struct list_node *newNode = new_node(value);
   for (struct list_node *cur = list.head; cur != NULL; cur = cur->next) {
      struct list_node *me = cur;
     
