b89994398f604f28fe89b955cdf682d1a266d8fd
GreenMacheen <46729245+greenmacheen@users.noreply.github.com>
2025-04-07 17:32:45 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -15,6 +15,24 @@ struct game_state dequeue(struct queue *q) {
 }
 
 
+#include "queue.h"
+#include "tile_game.h"
+#include "linked_list.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+void enqueue(struct queue *q, struct game_state state) {
+  insert_at_head(&(q->data), serialize(state));
+ 
+}
+
+struct game_state dequeue(struct queue *q) { 
+  
+  return deserialize(remove_from_head(&(q->data))); 
+}
+
+
 int number_of_moves(struct game_state start) {
   struct queue *q =  (struct queue *)malloc(sizeof(struct queue));
   struct game_state up;
@@ -26,24 +44,25 @@ int number_of_moves(struct game_state start) {
   //q->data.head = new_node(serialize(start));
   enqueue(q, start);
   while(true){
+      endNow = 0;
     up = dequeue(q);
     for (int i = 0; i < 16; i++ ) {
         tilecheck[i / 4][ i % 4] = i + 1;
     }
     for ( int j = 0; j < 15; j++ ) {
-        if (tilecheck[j/4][j%4] != up.tiles[j/4][j%4]) { 
+        
+        if (tilecheck[j/4][j%4] == up.tiles[j/4][j%4]) { 
             endNow++;
-          j = 15;
         }
     }
-    if(endNow == 1 || up.num_steps > 20) {
+    if(endNow > 14 || up.num_steps > 9) {
       //free_list(q->data);
       free(q);
       return up.num_steps;
     }
-    down = up;
-    left = up;
-    right = up;
+    memcpy(&down, &up, sizeof(struct game_state)) ;
+    memcpy(&left, &up, sizeof(struct game_state)) ;
+    memcpy(&right, &up, sizeof(struct game_state)) ;
     move_up(&up);
     move_down(&down);
     move_left(&left);
