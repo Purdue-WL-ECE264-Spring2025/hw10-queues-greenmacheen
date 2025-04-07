0cfbdf9b974405c296fd8597383b3f11c5219947
GreenMacheen <46729245+greenmacheen@users.noreply.github.com>
2025-04-07 18:53:58 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -22,6 +22,7 @@ struct game_state dequeue(struct queue *q) {
 int number_of_moves(struct game_state start) {
   struct queue *q =  (struct queue *)malloc(sizeof(struct queue));
   struct linked_list *list = (struct linked_list *)malloc(sizeof(struct linked_list));
+  insert_at_tail(&(list), serialize(state))
   struct game_state up;
   struct game_state down;
   struct game_state left;
@@ -30,7 +31,6 @@ int number_of_moves(struct game_state start) {
   int endNow = 0;
   int step = 0;
   q->data = *list;
-  enqueue(q, start);
   while(true){
       endNow = 0;
     up = dequeue(q);
