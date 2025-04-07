3a41895272fd135924ffca2e1a48d16d3cb9ca88
GreenMacheen <46729245+greenmacheen@users.noreply.github.com>
2025-04-06 20:02:13 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -24,7 +24,7 @@ int number_of_moves(struct game_state start) {
   struct game_state right;
   uint8_t tilecheck[4][4];
   int endNow = 1;
-  
+  enqueue(q, up);
   while(true){
     up = dequeue(q);
     for (int i = 0; i < 16; i++ ) {
