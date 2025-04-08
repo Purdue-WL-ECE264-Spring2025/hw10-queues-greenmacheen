020b02857d86a6f31faeb28148e3e9290f2b4cd1
GreenMacheen <46729245+greenmacheen@users.noreply.github.com>
2025-04-08 15:16:44 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -45,6 +45,7 @@ int number_of_moves(struct game_state start) {
     }
     if(endNow > 14 || up.num_steps > 9) {
       free_list(q->data);
+      free(q->data);
       free(q);
       return up.num_steps;
     }
